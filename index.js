1d8e61b58c19dd286a0facb3bd61d1431f319e58
witsanu <witsanu.uc@gmail.com>
2019-01-21 15:26:33 +0700

    add message reply

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT||5000
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+    channelAccessToken: 'ANOX1QMOTFBIbkZJfyAd/seoFWVhEhtOwzgiqi3HZkmoJSzyTTyl2darpUtsVKZj+Ytm1FjWviHZHFmGQ26Ue/Ar3PNsFVFn3D/AG9p+8exS5I633TiLLhEbdcWpxG8tB5Dj3w6ZOK7gHchYTHsuVAdB04t89/1O/w1cDnyilFU='
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -16,23 +21,94 @@ app.post('/webhook',(req,res) =>{
     let events = body.events[0];
     let source = events.source;
     let message = events.message;
+    let type = events.type;
+    let type = events.replyToken;
     console.log(`body ===> `);
     console.log(body); 
     console.log(`source ===> `);
     console.log(source);
     console.log(`message ===> `);
     console.log(message);
+    console.log(`type ===> `);
+    console.log(type);
+    console.log(`replyToken ===> `);
+    console.log(replyToken);
+
+switch(type){
+    case 'message':
+    
+    let type = message.type;
+    console.log(`[message type] ===> ${type}`);
+    let id = message.id;
+        if(type =='text'){
+            let text = message.text;
+            const message = [
+            {
+                type: 'text',
+                text: 'Moss สวัสดีครับ'
+            },
+            {
+                type: 'sticker',
+                stickerId: '2',
+                packageId: '1'
+            }
+        
+        ];
+            
+           replyMessage(replyToken, message)
+            }
+
+        else if(type =='sticker'){
+            let stickerId = message.stickerId;
+            let packageId = message.packageId;
+
+        }
+
+    break;
+    case 'follow':
+    break;
+    case 'unfollow':
+    break;
+    case 'join':
+    break;
+    case 'leave':
+    break;
+    default:
+    break;
+}
+
+
+
+
+
+
     let respon = {
         status : 'ok',
         body : body
 
     }
-console.log('Body ====>');
-console.log(body)
-
 
    res.send(respon);
 });
+
+
+//Method
+
+
+const replyMessage = (replyToken, message)=>{
+    console.log(` ===> [replyMessage]`);
+    console.log(` ===> replyToken: ${replyToken}`);
+    console.log(`message ===> `);
+    console.log(message);
+
+    client.replyMessage (replyToken, message)
+        .then(() => {
+            console.log('replyMessage is successfully!');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
 
 app.listen(port,() =>{
  console.log(`listen on port ${port}`)
