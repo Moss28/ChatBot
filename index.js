7c9efc85f090e6c7fbd74d8cc4e399eaeda768ee
witsanu <witsanu.uc@gmail.com>
2019-01-21 12:30:09 +0700

    add index.sj

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express();
+const port = process.env.POST||5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.get('/',(req,res) =>{
+    res.send({status: 'ok'});
+   });
+
+app.post('/webhook',(req,res) =>{
+    let body = req.body;
+    let respon = {
+        status : 'ok',
+        body : body
+    }
+console.log('Body ====>');
+console.log(body)
+
+
+   res.send(respon);
+});
+
+app.listen(port,() =>{
+ console.log(`listen on port ${port}`)
+});
